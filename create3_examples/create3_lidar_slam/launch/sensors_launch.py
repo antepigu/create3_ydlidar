fb6a9255a71f1aa2ec4006f0c3f1d8398148fd9c
antepigu <134631890+antepigu@users.noreply.github.com>
2023-10-28 09:52:19 -0500

    Update sensors_launch.py Use of params file

--- create3_examples/create3_lidar_slam/launch/sensors_launch.py
+++ create3_examples/create3_lidar_slam/launch/sensors_launch.py
@@ -3,7 +3,7 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, TimerAction
+from launch.actions import DeclareLaunchArgument, TimerAction, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 import os
